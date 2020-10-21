f27c68c4ee6f4b359d90ec890f7de2520b1deb7c
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2020-10-21 13:13:35 -0400

    first commit

---
+++ src/main/java/com/appsdeveloperblog/oauth/SpaExampleApplication.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appsdeveloperblog/oauth/SpaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
